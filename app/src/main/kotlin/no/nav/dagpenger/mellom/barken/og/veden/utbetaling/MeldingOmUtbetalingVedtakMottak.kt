6aebacb62e0f9c054edc98f175eb70d19eb9efc5
Geir A. Lund <geir.andre.lund@nav.no>
2026-09-14 08:58:16 +0200

    Hopper over behandling som er lagret fra før. 01a09cc9-5a91-72ec-82e0-dec0c07cc187 Ref hendelse #dagpenger-hendelse-2027-09-utbetaling-forsinkelse
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>

--- app/src/main/kotlin/no/nav/dagpenger/mellom/barken/og/veden/utbetaling/MeldingOmUtbetalingVedtakMottak.kt
+++ app/src/main/kotlin/no/nav/dagpenger/mellom/barken/og/veden/utbetaling/MeldingOmUtbetalingVedtakMottak.kt
@@ -60,7 +60,7 @@ internal class MeldingOmUtbetalingVedtakMottak(
         ) {
             logger.info { "Mottok melding om utbetaling for meldekort" }
 
-            if (behandlingId.toString() == "01a09cc9-591a-72c7-b2e8-b81740dea3a6" && System.getenv()["NAIS_CLUSTER_NAME"] == "prod-gcp") {
+            if (behandlingId.toString() == "01a09cc9-5a91-72ec-82e0-dec0c07cc187" && System.getenv()["NAIS_CLUSTER_NAME"] == "prod-gcp") {
                 logger.info { "Hopper over behandling med ID $behandlingId" }
                 return@withLoggingContext
             }
